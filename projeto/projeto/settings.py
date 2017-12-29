2f89fd206e5b680aa7d2aef5c132f831d5b0d88e
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2017-12-28 23:07:57 -0200

    Tentando resolver o problema de autenticação, mas ainda sem sucesso

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3vg%7%_ud-_(p$h1@+1(trczbf5)exb#479qp1xnobs&y-a05@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.7']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 AUTH_USER_MODEL = 'usuario.Usuario'
 
